90d9b17de6f44aaf1c6b18b899d9a4beec6a5367
Matthew Okadinya <matthewokadinya@gmail.com>
2022-09-27 13:46:30 +0100

    new event

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 // document.getElementById("count-emt").innerText = 5
 // to build our passenger counter app we need some waay to store the data
-// to sore data we need to create a variable
+// to store data we need to create a variable
 
-// let count = 5 
-// to reassign
-// count += 4
-// console.log(count)
-
-let bonusPoints = 50
-
-bonusPoints += 50
-
-bonusPoints -= 75
-
-bonusPoints += 45
-
-
-
-console.log(bonusPoints)
+// Initialize the count as 0
+// listen for clicks on the increment button by using the "on-click" event listener
+// increment the count variable when the button is clicked
+// to change the "count-emt" in the html to reflect the new count
 
