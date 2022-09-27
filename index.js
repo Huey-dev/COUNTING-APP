163fb38e3e7b7ce07d7c81986d5aac9a4b223ac6
Matthew Okadinya <matthewokadinya@gmail.com>
2022-09-27 13:32:10 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// document.getElementById("count-emt").innerText = 5
+// to build our passenger counter app we need some waay to store the data
+// to sore data we need to create a variable
+
+// let count = 5 
+// to reassign
+// count += 4
+// console.log(count)
+
+let bonusPoints = 50
+
+bonusPoints += 50
+
+bonusPoints -= 75
+
+bonusPoints += 45
+
+
+
+console.log(bonusPoints)
+
