c284103edcebe917ef647e815d045f4c6a04a528
Matthew Okadinya <matthewokadinya@gmail.com>
2022-09-28 07:47:10 +0100

    great! our web counter app is live and responsive

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 // to build our passenger counter app we need some waay to store the data
 // to store data we need to create a variable
 // javascriptvariables are written in camel case
-let countElement = document.getElementById("count-emt")
+let countElement = document.getElementById("count-emt") //pass an arguement
 
-console.log(countElement)
+// we grab the save-emt paragraph and store it in a variable called saveElement
+
+let saveElement = document.getElementById("save-emt")
+
+
 
 // Initialize the count as 0
 
@@ -17,8 +21,18 @@ function increment() {
 // increment the count variable when the button is clicked
 
     count += 1;
-    countElement.innerText = count
+    countElement.innerText = count;
 }
 
 // to change the "count-emt" in the html to reflect the new count
+
+function save() {
+    // create a variable that contains both the count and the dash separator, i.e. "13 - "
+    let countStr = count + " " + " - ";
+    // render the variable in the saveElement using the innerText
+    saveElement.innerText += countStr
+    console.log(count)
+}
+
+
 
